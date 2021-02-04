8ee54806d46c6c704c86673a5d6932142f01d4a7
Jessywu195 <1531733078@qq.com>
2021-02-04 21:39:29 +0800

    add Font Adapter

--- ClockExp/ClockExp/RecordCell.m
+++ ClockExp/ClockExp/RecordCell.m
@@ -5,6 +5,10 @@
 //  Created by Ellie Weng on 2021/1/28.
 //  Copyright © 2021 Jessie Wu. All rights reserved.
 //
+#define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
+#define FONT_SIZE(size) ([UIFont systemFontSize:FontSize(size))])
+
+
 
 #import "RecordCell.h"
 #define MAS_SHORTHAND
@@ -14,14 +18,12 @@
 #import "Masonry.h"
 #import "TimeModel.h"
 
-#define CELL_MARGIN 10
 extern CGFloat cellEdgeOffset;
 
 @interface RecordCell ()
 
 @property(nonatomic,weak)UILabel * recordCount;
 @property(nonatomic,weak)UILabel * timeLabel;
-//@property(nonatomic,strong)RecordModel * cellData;
 
 @end
 
@@ -32,6 +34,7 @@ UIColor *backgroundColor;
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+   
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         UILabel *recordCount = [[UILabel alloc] init];
         UILabel *timeLabel = [[UILabel alloc]init];
@@ -47,18 +50,15 @@ UIColor *backgroundColor;
             make.centerY.equalTo(self.contentView);
         }];
 
-        timeLabel.font = [UIFont monospacedDigitSystemFontOfSize:17 weight:UIFontWeightRegular];    //设置timeLabel的字体大小
-        
+//        CGFloat standardFont = FONT_SIZE(17);
+        timeLabel.font = [UIFont monospacedDigitSystemFontOfSize:17 weight:UIFontWeightRegular]; //设置timeLabel的字体大小
+
         //timeLabel位置约束
         [timeLabel makeConstraints:^(MASConstraintMaker *make) {
             make.right.equalTo(self.contentView.mas_right).offset(-cellEdgeOffset);
             make.centerY.equalTo(self.contentView);
-//            make.width.equalTo(100);
-//            make.height.equalTo(recordCount);
         }];
-        
         self.selectionStyle = UITableViewCellSelectionStyleNone;    //设置cell无法选中
-//        self.contentView.backgroundColor = backgroundColor;         //设置contenView的背景颜色
         self.backgroundColor = backgroundColor;                     //设置tableView的背景颜色
     }
     
@@ -75,4 +75,14 @@ UIColor *backgroundColor;
     self.timeLabel.textColor = cellData.recordColor;
 }
 
+
+static inline CGFloat FontSize(CGFloat fontSize)
+{
+    if (SCREEN_WIDTH == 375) {
+        return fontSize;
+    }
+//    else if(SCREEN_WIDTH == 414){
+        return (fontSize + 1);
+//    }
+}
 @end
